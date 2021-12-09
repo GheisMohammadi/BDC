6402eba72bbe90615874298cc74ac2faaad58468
GheisMohammadi <gheis@lifelinelab.io>
2021-12-09 14:11:57 +0800

    added PoW

--- src/transaction/transaction.go
+++ src/transaction/transaction.go
@@ -4,13 +4,40 @@ import (
 	hash "badcoin/src/helper/hash"
 	"encoding/hex"
 	"encoding/json"
+	"fmt"
+	"strings"
 )
 
 type Transaction struct {
+	ID       hash.Hash
 	Sender   string
 	Receiver string
 	Amount   uint64
 	Memo     string
+}
+
+// String returns a human-readable representation of a transaction
+func (tx Transaction) String() string {
+	var lines []string
+
+	lines = append(lines, fmt.Sprintf("--- Transaction %v:", tx.ID))
+
+	// for i, input := range tx.Inputs {
+
+	// 	lines = append(lines, fmt.Sprintf("     Input %d:", i))
+	// 	lines = append(lines, fmt.Sprintf("       TXID:         %v", input.PreviousOutPoint.Hash.String()))
+	// 	lines = append(lines, fmt.Sprintf("       OutIndex:     %d", input.PreviousOutPoint.Index))
+	// 	lines = append(lines, fmt.Sprintf("       Signature:    %x", input.Signature))
+	// 	lines = append(lines, fmt.Sprintf("       PubKey:       %x", input.PubKey))
+	// }
+
+	// for i, output := range tx.Outputs {
+	// 	lines = append(lines, fmt.Sprintf("     Output %d:", i))
+	// 	lines = append(lines, fmt.Sprintf("       Value:        %d", output.Value))
+	// 	lines = append(lines, fmt.Sprintf("       PubKeyHash:   %x", output.PubKeyHash))
+	// }
+
+	return strings.Join(lines, "\n")
 }
 
 func (tx *Transaction) Serialize() []byte {
